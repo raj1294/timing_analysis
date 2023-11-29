c0d2d734b87f1f766eb0fb6e0914120de8b81243
raj1294 <79147737+raj1294@users.noreply.github.com>
2023-11-29 14:29:18 +0000

    Update phase_model.py

--- phase_model.py
+++ phase_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Nov 17 13:00:19 2023
+Created on Wed Nov 29 02:24:09 2023
 
 @author: erc_magnesia_raj
 """
@@ -10,87 +10,83 @@ import matplotlib.pyplot as plt
 from stingray import EventList, Lightcurve
 from stingray.pulse.search import phaseogram, plot_phaseogram
 from matplotlib.gridspec import GridSpec
+from scipy.optimize import fsolve
+
+day = 86400 
+c = 3e8 #Spped of light
+ls = 3e8 #Light-seconds
 
 #Generate signal phase map
-def simple_phase_model(tdur,dt,ntimes,nphase,spinpar,mu,pf,fout):
+def phase_model(tdur,dt,ntimes,nphase,spinpar,orbpar,\
+                       mu,pf,fout):
     
     freq,fdot,phioff = spinpar
+    Porb,ap,ecc,omega,T0 = orbpar
+    
     time = np.arange(0,tdur,dt)
+    M = (2.0*np.pi/Porb)*(time-T0)
+    E = np.zeros(len(M))
+    
+    for k in range(len(M)):
+        
+        def func(x):
+            return x - ecc*np.sin(x) - M[k]
+        
+        sol = fsolve(func,0.0)[0]
+        E[k] = sol
+    
+    arg = np.sqrt((1+ecc)/(1-ecc))*np.tan(E/2.)
+    f = 2.0*np.arctan(arg)
+
+    #Line-of-sight velocity
+    vl = (2.0*np.pi/Porb)*(ap)/(np.sqrt(1-ecc**2))*(np.cos(f+omega)+\
+                                                    ecc*np.cos(omega))
     rate = np.zeros(len(time))
     for j in range(len(rate)):
-        fevol = freq + fdot*time[j]
+        fevol = freq*(1 + vl[j]/c) + fdot*time[j]
         rate[j] = mu*(1 + pf*np.cos(2.0*np.pi*fevol*time[j] + phioff))
     counts = np.random.poisson(rate*dt)
+    time = time[0:-1]
+    counts = counts[0:-1]
+    vl = vl[0:-1]
     lc = Lightcurve(time,counts,dt=dt)
     ev = EventList()
     ev = ev.from_lc(lc)
+    ev.write("events.fits","fits")
+    
+    plt.figure()
+    gs = GridSpec(1, 1)
+    ax0 = plt.subplot(gs[0])
+    phaseogr, phases, times, info = \
+    phaseogram(ev.time, freq, return_plot=True, nph=nphase, nt=ntimes)
+    _ = plot_phaseogram(phaseogr, phases, times, ax=ax0,\
+                        vmin=np.median(phaseogr))
         
-    plt.figure()
-    gs = GridSpec(1, 1)
-    ax0 = plt.subplot(gs[0])
-    phaseogr, phases, times, info = \
-    phaseogram(ev.time, freq, return_plot=True, nph=nphase, nt=ntimes)
-    _ = plot_phaseogram(phaseogr, phases, times, ax=ax0,\
-                        vmin=np.median(phaseogr))
     plt.xticks([])
     plt.yticks([])
-    dirout = "phase_maps/" + fout
+    dirout = fout
     plt.savefig(dirout,bbox_inches='tight',pad_inches=0)
 
-#Generate random noise phase map
-def poisson_noise(tdur,dt,spinpar):
-    
-    freq,fdot,phioff = spinpar
-    time = np.arange(0,tdur,dt)
-    rate = np.zeros(len(time))
-    for j in range(len(rate)):
-        rate[j] = mu
-    counts = np.random.poisson(rate*dt)
-    lc = Lightcurve(time,counts,dt=dt)
-    ev = EventList()
-    ev = ev.from_lc(lc)    
-    
-    plt.figure()
-    gs = GridSpec(1, 1)
-    ax0 = plt.subplot(gs[0])
-    phaseogr, phases, times, info = \
-    phaseogram(ev.time, freq, return_plot=True, nph=nphase, nt=ntimes)
-    _ = plot_phaseogram(phaseogr, phases, times, ax=ax0,\
-                        vmin=np.median(phaseogr))
-    dirout = "noise/" + fout
-    plt.xticks([])
-    plt.yticks([])
-    plt.savefig(dirout,bbox_inches='tight',pad_inches=0)
+#Observation parameters
+tdur = 90e3 #Duration of observation
+dt = 0.1 #Binning time
+mu = 2.0 #Mean count-rate
+pf = 0.5 #Pulsed fraction
+ntimes = 96 #Number of time bins in phaseogram
+nphase = 48 #Number of phase bins in phaseogram
+fout = "phase_ecc.png" #Output file (for phaseogram)
 
-#Fake model parameters
-tdur = 30e3
-dt = 0.07336496
-ntimes = 100
-nphase = 16
-poffset = np.random.uniform(0.0,2.0*np.pi,1)[0]
-spinpar = [0.68,0.0,poffset]
-ntrain = 2000
-#Generate training set (signal)
-for j in range(ntrain):
-    mu = np.random.uniform(0.2,2.0,1)[0]
-    pf = np.random.uniform(0.1,0.9,1)[0]
-    fout = "phase" + str(j+1) + "_train.png"
-    simple_phase_model(tdur,dt,ntimes,nphase,spinpar,mu,pf,fout)
-#Generate training set (noise)
-for k in range(ntrain):
-    mu = np.random.uniform(0.2,2.0,1)[0]
-    fout = "noise" + str(k+1) + "_train.png"
-    poisson_noise(tdur,dt,spinpar)
+#Binary parameters
+Porb = 1.0*day #Orbital period
+ecc = 0.2 #Eccentricity
+ap = 2.0*ls #Semi-major axis
+T0 = 0.0*day #Epoch of periastron
+omega = np.pi/2. #Longitude of periastron
+binpar = [Porb,ap,ecc,omega,T0]
 
-ntest = 200
-#Generate testing set (signal)
-for j in range(ntest):
-    mu = np.random.uniform(0.2,2.0,1)[0]
-    pf = np.random.uniform(0.1,0.9,1)[0]
-    fout = "phase" + str(j+1) + "_test.png"
-    simple_phase_model(tdur,dt,ntimes,nphase,spinpar,mu,pf,fout)
-#Generate testing set (noise)
-for k in range(ntest):
-    mu = np.random.uniform(0.2,2.0,1)[0]
-    fout = "noise" + str(k+1) + "_test.png"
-    poisson_noise(tdur,dt,spinpar)
+#Spin parameters
+freq = 0.68 #Spin frequency
+fdot = -6.6e-11 #Frequency derivative
+phioff = 0.0 #Phase offset
+spinpar = [freq,fdot,phioff]
+counts = phase_model(tdur,dt,ntimes,nphase,spinpar,binpar,mu,pf,fout)
