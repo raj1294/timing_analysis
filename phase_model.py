9ab6700cbf6c01bb5b21dcb0a7215a54ab0752d6
raj1294 <79147737+raj1294@users.noreply.github.com>
2023-11-29 17:53:51 +0000

    Update phase_model.py

--- phase_model.py
+++ phase_model.py
@@ -11,12 +11,13 @@ from stingray import EventList, Lightcurve
 from stingray.pulse.search import phaseogram, plot_phaseogram
 from matplotlib.gridspec import GridSpec
 from scipy.optimize import fsolve
+from scipy import integrate
 
 day = 86400 
 c = 3e8 #Spped of light
 ls = 3e8 #Light-seconds
 
-#Generate signal phase map
+#Generate signal phase map (Keplerian eccentric orbits)
 def phase_model(tdur,dt,ntimes,nphase,spinpar,orbpar,\
                        mu,pf,fout):
     
@@ -25,33 +26,39 @@ def phase_model(tdur,dt,ntimes,nphase,spinpar,orbpar,\
     
     time = np.arange(0,tdur,dt)
     M = (2.0*np.pi/Porb)*(time-T0)
-    E = np.zeros(len(M))
-    
+    E = np.zeros(len(time))
+    rl = np.zeros(len(time))
+    rate = np.zeros(len(time))
+    rl0 = 0
     for k in range(len(M)):
         
         def func(x):
             return x - ecc*np.sin(x) - M[k]
         
-        sol = fsolve(func,0.0)[0]
-        E[k] = sol
+        sol = fsolve(func,0.0)
+        E[k] = sol[0]
     
-    arg = np.sqrt((1+ecc)/(1-ecc))*np.tan(E/2.)
-    f = 2.0*np.arctan(arg)
-
-    #Line-of-sight velocity
-    vl = (2.0*np.pi/Porb)*(ap)/(np.sqrt(1-ecc**2))*(np.cos(f+omega)+\
-                                                    ecc*np.cos(omega))
-    rate = np.zeros(len(time))
-    for j in range(len(rate)):
-        fevol = freq*(1 + vl[j]/c) + fdot*time[j]
-        rate[j] = mu*(1 + pf*np.cos(2.0*np.pi*fevol*time[j] + phioff))
+        arg = np.sqrt((1+ecc)/(1-ecc))*np.tan(E[k]/2.)
+        f = 2.0*np.arctan(arg)
+    
+        #Line-of-sight distance
+        rl[k] = ap*(1-ecc**2)*((1 + ecc*np.cos(f))**-1)*np.sin(f + omega)
+        
+        if(k==0):
+            rl0 = rl[k]
+        
+        phase_orb = (rl[k] - rl0)/c
+        phase_spin = 0.5*fdot*time[k]**2
+        
+        #Waveform
+        rate[k] = mu*(1 + pf*np.cos(2.0*np.pi*freq*(time[k]+phase_orb)+\
+                                    phase_spin + phioff))
+        
     counts = np.random.poisson(rate*dt)
-    time = time[0:-1]
-    counts = counts[0:-1]
-    vl = vl[0:-1]
     lc = Lightcurve(time,counts,dt=dt)
     ev = EventList()
     ev = ev.from_lc(lc)
+    ev.time += 207850374.816834
     ev.write("events.fits","fits")
     
     plt.figure()
@@ -61,7 +68,6 @@ def phase_model(tdur,dt,ntimes,nphase,spinpar,orbpar,\
     phaseogram(ev.time, freq, return_plot=True, nph=nphase, nt=ntimes)
     _ = plot_phaseogram(phaseogr, phases, times, ax=ax0,\
                         vmin=np.median(phaseogr))
-        
     plt.xticks([])
     plt.yticks([])
     dirout = fout
@@ -79,9 +85,9 @@ fout = "phase_ecc.png" #Output file (for phaseogram)
 #Binary parameters
 Porb = 1.0*day #Orbital period
 ecc = 0.2 #Eccentricity
-ap = 2.0*ls #Semi-major axis
+ap = 0.5*ls #Semi-major axis
 T0 = 0.0*day #Epoch of periastron
-omega = np.pi/2. #Longitude of periastron
+omega = 0.0 #Longitude of periastron
 binpar = [Porb,ap,ecc,omega,T0]
 
 #Spin parameters
@@ -89,4 +95,5 @@ freq = 0.68 #Spin frequency
 fdot = -6.6e-11 #Frequency derivative
 phioff = 0.0 #Phase offset
 spinpar = [freq,fdot,phioff]
-counts = phase_model(tdur,dt,ntimes,nphase,spinpar,binpar,mu,pf,fout)
+counts = phase_model(tdur,dt,ntimes,nphase,spinpar,binpar,mu,pf,\
+                            fout)
